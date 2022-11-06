4c253f98644150f77bdf0346935c4d701303bfe9
putsfh <563213851@qq.com>
2022-11-06 21:54:17 +0800

    移动端加上vconsole

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import zhCn from "element-plus/es/locale/lang/zh-cn"; //解决elementpLus一些组件是英文的问题
 
+import Vconsole from "vconsole"; //调试
+import.meta.env.VITE_APP_NODE_ENV === "development" ? new Vconsole() : "";
 import { createPinia } from "pinia";
 const pinia = createPinia();
 
